dffcd27ed57e3e7852d8c9c4bad6edf55c4ff044
kitaindia <morisot64@gmail.com>
2016-07-13 00:13:57 +0900

    登録時にユーザー名も入力させる

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,7 @@ class User < ApplicationRecord
 
   has_many :user_applists
   has_many :applists, through: :user_applists
+
+  validates_uniqueness_of :username
+  validates_presence_of :username
 end
