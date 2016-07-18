49b49fc67b0819ec9ff576fc260f61bb57d553aa
kitaindia <morisot64@gmail.com>
2016-07-19 01:30:08 +0900

    urlにvalidator設定

--- app/models/applist.rb
+++ app/models/applist.rb
@@ -3,4 +3,8 @@ class Applist < ApplicationRecord
   has_many :users, through: :user_applists
   has_many :itunes_apps
   has_many :google_play_apps
+
+  validates :google_play_url, allow_blank: true, format: /\A#{URI::regexp(%w(http https))}\z/
+  validates :itunes_url, allow_blank: true, format: /\A#{URI::regexp(%w(http https))}\z/
+  validates_with AnyFieldFillValidator, fields: [:google_play_url, :itunes_url]
 end
