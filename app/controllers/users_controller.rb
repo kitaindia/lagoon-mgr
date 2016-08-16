c880fb15ee8fd00f00df18791192e59a046509e4
kitaindia <morisot64@gmail.com>
2016-08-17 01:44:56 +0900

    管理人がユーザーを登録・編集・削除できるようなフォームを用意

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,70 @@
 class UsersController < ApplicationController
 
-  before_action :authenticate_user!, :authenticate_admin_user!
+  before_action :authenticate_user!
+  before_action :authenticate_admin_user!
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   def index
-    @users = User.all
+    @users = User.where(is_admin: false)
+  end
+
+  def show
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def edit
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to @user, notice: 'User was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+
+  def update
+    if params[:user][:password].blank?
+      params[:user].delete(:password)
+      params[:user].delete(:password_confirmation)
+    end
+
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+  def destroy
+    @user.destroy
+    respond_to do |format|
+      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
+    end
   end
 
   private
-  def authenticate_admin_user!
-    unless current_user.is_admin
-      raise ActionController::RoutingError.new('Not Found')
+    def authenticate_admin_user!
+      unless current_user.is_admin
+        raise ActionController::RoutingError.new('Not Found')
+      end
     end
-  end
+
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(:username, :email, :password, :password_confirmation)
+    end
 end
