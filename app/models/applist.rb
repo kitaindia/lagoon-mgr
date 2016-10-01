0c898e7b23f8ef9abcb2595cb8ecd3b6e24a329e
kitaindia <morisot64@gmail.com>
2016-10-01 22:26:56 +0900

    scrape_app をtransaction内で実行

--- app/models/applist.rb
+++ app/models/applist.rb
@@ -11,80 +11,55 @@ class Applist < ApplicationRecord
 
   def scrape_app
 
-    itunes_result = self.fetch_itunes_app
-    google_play_result = self.fetch_google_play
+    Applist.transaction do
+      itunes_url.present? && fetch_itunes_app.save!
+      google_play_url.present? && fetch_google_play.save!
 
-    #
-    # nil... means failed to scrape app detail
-    #
-    return false if itunes_result.nil? || google_play_result.nil?
-
-    if itunes_result || google_play_result
-
-      itunes_result.save if itunes_result
-      google_play_result.save if google_play_result
-
-      self.update(is_scraped: true)
-      return true
-    else
-       return false
+      update(is_scraped: true)
     end
+    rescue => e
+      puts e
+      return
   end
 
   def fetch_itunes_app
     #
     # fetch app data from itunes app API
     #
-    return false unless self.itunes_url
+    params = itunes_app_params
+    return unless params
 
-    country_code = itunes_url[/(?<=itunes\.apple\.com\/)[^\/]+/]
-    app_id = itunes_url[/(?<=id)[\d]+/]
-
-    return nil if country_code.nil? or app_id.nil?
-
-    if country_code == 'en'
-      # TODO Convert ISO 639-2 Language Code
-      country_code = 'us'
+    begin
+      response = open("https://itunes.apple.com/#{params[:country_code]}/lookup?id=#{params[:app_id]}")
+      code, message = response.status
+      if code == '200'
+        json = ActiveSupport::JSON.decode(response.read)
+        result = json['results'][0]
+        itunes_app = ItunesApp.new(
+          icon_url: result['artworkUrl100'],
+          name: result['trackName'],
+          applist_id: self.id
+        )
+      end
     end
-
-    response = open("https://itunes.apple.com/#{country_code}/lookup?id=#{app_id}")
-    code, message = response.status
-    if code == '200'
-      json = ActiveSupport::JSON.decode(response.read)
-      result = json['results'][0]
-      return ItunesApp.new(
-        icon_url: result['artworkUrl100'],
-        name: result['trackName'],
-        applist_id: self.id
-      )
-    end
-
-    return nil
   end
 
   def fetch_google_play
     #
     # fetch data from google play Web site
     #
-    return false unless self.google_play_url
+    params = google_play_params
+    return unless params
 
-    app_id = self.google_play_url[/(?<=id=)[\S]+$/]
-
-    return nil if app_id.nil?
-
-    app = MarketBot::Play::App.new(app_id)
+    app = MarketBot::Play::App.new(params[:app_id])
     begin
       app.update
-      return GooglePlayApp.new(
+      google_play_app = GooglePlayApp.new(
         icon_url: app.cover_image_url,
         name: app.title,
         applist_id: self.id
       )
-    rescue MarketBot::NotFoundError => e
-      puts e.message
     end
-
-    return nil
   end
 
   def self.import(rows)
@@ -101,6 +76,8 @@ class Applist < ApplicationRecord
     applists
   end
 
+  private
+
   def must_be_itunes_url
     return if itunes_url.nil?
 
@@ -116,4 +93,33 @@ class Applist < ApplicationRecord
       errors.add(:google_play_url, 'google_play_url field must be google_play_url')
     end
   end
+
+  def itunes_app_params
+
+    return unless itunes_url
+
+    country_code = itunes_url[/(?<=itunes\.apple\.com\/)[^\/]+/]
+    app_id = itunes_url[/(?<=id)[\d]+/]
+
+    return if country_code.nil? or app_id.nil?
+
+    if country_code == 'en'
+      # TODO Convert ISO 639-2 Language Code
+      country_code = 'us'
+    end
+
+    {country_code: country_code, app_id: app_id}
+  end
+
+  def google_play_params
+
+    return unless google_play_url
+
+    app_id = google_play_url[/(?<=id=)[\S]+$/]
+
+    return if app_id.nil?
+
+    {app_id: app_id}
+  end
+
 end
